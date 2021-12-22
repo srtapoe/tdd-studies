e6f0e23678e8aa53b73936c2e26be4b7a926dc71
jhoaniceoliveira@gmail.com <jhoaniceoliveira@gmail.com>
2021-12-22 16:49:26 -0300

    atualizando trabalho da disciplina de QS

--- notas-alunos/src/main/java/com/oliveira/notas_alunos/Aluno.java
+++ notas-alunos/src/main/java/com/oliveira/notas_alunos/Aluno.java
@@ -6,74 +6,83 @@ public class Aluno {
 
 	//resolução com tudo dentro menos o calculo de todas as medias
 	//e mostrar todas as médias dos alunos cadastrados
-	public static void main(String[] args) {
-		Scanner dadosEntrada = new Scanner(System.in);
+
+	Scanner dadosEntrada = new Scanner(System.in);
 			//variaveis a serem usadas
-			double nota1, nota2, nota3;
-			double mediaAluno, mediaTurma, menorNota;
-			String matricula;
-			int qtdeAlunos, i = 0, alunosCadastrados;
-			
-			//entrada da quantidade de alunos
-				System.out.println("====Máximo 5 alunos====");
-				System.out.print("Digite a quantidade de alunos: ");
-				qtdeAlunos = dadosEntrada.nextInt();
+		double nota1, nota2, nota3, qtdeAlunos;
+		double mediaAluno;
+		double mediaAcumulada = 0;
+		double menorNota;
+		double mediaAlunos = 0;
+		String matricula;
+		int i;
+		
+		qtdeAlunos = dadosEntrada.nextInt();
 				
-			//dados dos alunos
-				int j = 0;
-				while(j < qtdeAlunos) {
+	//dados dos alunos
+double receberNotas() {			
+		int j = 0;
+		while(j < qtdeAlunos) {
 				
 					
-					System.out.println("Matricula do aluno: ");
-					matricula = dadosEntrada.next();
+		System.out.println("Matricula do aluno: ");
+		matricula = dadosEntrada.next();
 					
 			        // recebe a 1º nota
-			        System.out.print("1° nota do aluno " + matricula + ": ");
-			        nota1 = dadosEntrada.nextDouble();
+		System.out.print("1° nota do aluno " + matricula + ": ");
+		nota1 = dadosEntrada.nextDouble();
 			        
 			        // recebe a 2º nota
-			        System.out.print("2° nota do aluno " + matricula + ": ");
-			        nota2 = dadosEntrada.nextDouble();
+	   System.out.print("2° nota do aluno " + matricula + ": ");
+	   nota2 = dadosEntrada.nextDouble();
 			        
 			        // recebe a 3º nota
-			        System.out.print("3° nota do aluno " + matricula + ": ");
-			        nota3 = dadosEntrada.nextDouble();
+	   System.out.print("3° nota do aluno " + matricula + ": ");
+	   nota3 = dadosEntrada.nextDouble();
 			        
 			        // calcula a média
-			        mediaAluno = (nota1 + nota2 + nota3) / 3;
-			        
-					
-			        double notas[] = {nota1, nota2,nota3};
-			        menorNota = notas[0];
-			        for(i = 0; i<notas.length;i++) {
-			        	if(notas[i] < menorNota) {
-			        		menorNota = notas[i];
-			        	}
-			        }
-			        
-			        
-			        
-					//solicita uma quarta nota se a média for menor que 7
-			        if(mediaAluno < 7) {
-			        	System.out.print("Digite a nota substitutiva: ");
-			        	double nota4 = dadosEntrada.nextDouble();
-			        	if(nota4 !=nota1 && nota4 !=nota2 && nota4 <= nota3) {
-			        		double somatorio = (nota1 + nota2 + nota3 + nota4) - menorNota;
-				        	mediaAluno = somatorio/3;
-			        	}
-			        	System.out.printf("A média do aluno %s é %.2f \n ",matricula, mediaAluno);
-			        }else {
-			        	mediaAluno = (nota1 + nota2 + nota3) / 3;
-			        	System.out.printf("A média do aluno  %s é %.2f \n ", matricula, mediaAluno);
-			        }
-			        
-			        
-					j++;
+	   mediaAluno = (nota1 + nota2 + nota3) / 3;
+			        j++;
 				}
-		}
-		
-		
-		
-		
+			        
+}					
+
+double verificarMedia() {
+	double notas[] = {nota1, nota2,nota3};
+	  menorNota = notas[0];
+	  for(i = 0; i<notas.length;i++) {
+		if(notas[i] < menorNota) {
+		menorNota = notas[i];
+			        	}
+			        }
+				     
+			            
+//solicita uma quarta nota se a média for menor que 7
+	if(mediaAluno < 7) {
+    System.out.print("Digite a nota substitutiva: ");
+	double nota4 = dadosEntrada.nextDouble();
+	if(nota4 !=nota1 && nota4 !=nota2 && nota4 <= nota3) {
+	 double somatorio = (nota1 + nota2 + nota3 + nota4) - menorNota;
+	  mediaAluno = somatorio/3;
+	  mediaAcumulada += mediaAluno;
+}
+			        	
+	System.out.printf("A média do aluno %s é %.2f \n ",matricula, mediaAluno);
+			        
+}else {
+		mediaAluno = (nota1 + nota2 + nota3) / 3;
+		mediaAcumulada += mediaAluno;
+	System.out.printf("A média do aluno  %s é %.2f \n ", matricula, mediaAluno);
+}
+			        
+			        
+	System.out.printf("Média da turma: %.2f \n ", mediaAcumulada / qtdeAlunos);
+
+}
 	}
+				
+
+		
+		
+
 
---
+++ notas-alunos/src/test/java/com/oliveira/notas_alunos/AlunoTeste.java
@@ -0,0 +1,29 @@
+package com.oliveira.notas_alunos;
+
+
+import org.junit.Test;
+
+public class AlunoTeste {
+
+	Aluno aluno = new Aluno();
+	
+	@Test
+	public void verificarRequisitosDaNotaquatro() {
+		//cenario
+		double nota1 = 5;
+		double nota2 = 8;
+		double nota3 = 9.5;
+		
+		double notas [] = {nota1, nota2, nota3};
+		
+		//execucao
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+}
